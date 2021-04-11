f70a06a7bb5c8d531e85c2403b530f9c50b6caca
Chintan Sudani <csudani7@gmail.com>
2021-04-12 02:07:35 +0530

    added search component

--- src/components/SearchInputBox/index.jsx
+++ src/components/SearchInputBox/index.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Input, Select, Radio, Checkbox } from 'antd';
+import { SearchOutlined, InfoCircleOutlined } from '@ant-design/icons';
+const InputGroup = Input.Group;
+const Option = Select.Option;
 
 export default function SearchInputBox() {
+
+  const [value, setValue] = useState('Exact');
+
+  const handleDataChange = (e) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+  }
+
   return (
     <div>
-      SearchInputBox
+      <InputGroup compact>
+        <Select style={{width: 90}} className="selectData" value={value}>
+          <Radio.Group onChange={handleDataChange} >
+          <Radio value='Exact'>
+          Exact Phrase
+          </Radio>
+          <hr />
+          <Radio value='All'>
+          All Word
+          <div>
+              within <Input type="number" value="20" style={{width: "60px"}} /> words
+            </div>
+          </Radio>
+          <hr />
+          <Radio value='Any'>
+          Any Word
+          </Radio>
+          <hr />
+          <Checkbox value='Synonyms'>Use Synonyms</Checkbox>
+          </Radio.Group>
+        </Select>
+        <Input
+          style={{ width: '286px' }}
+          suffix={comboSuffix}
+          placeholder="Email"
+        />
+      </InputGroup>
     </div>
   )
 }
+
+const comboSuffix = (
+  <>
+    <InfoCircleOutlined />&nbsp;<SearchOutlined />
+  </>
+)
+
