e3a2a6b426b9590e8162dfde02cd1734c427e331
Chintan Sudani <csudani7@gmail.com>
2021-04-11 18:25:23 +0530

    added page layout

--- src/components/TopBarHeader.jsx
+++ src/components/TopBarHeader.jsx
@@ -3,14 +3,27 @@ import SearchInputBox from "./SearchInputBox";
 import DocumentTypeSelect from "./DocumentTypeSelect";
 import DatePickerComponent from "./DatePickerComponent";
 import SelectCompanyBox from "./SelectCompanyBox";
+import "./TopBarHeader.css";
 
 export default function TopBarHeader() {
   return (
-    <div>
-      <SearchInputBox />
-      <DocumentTypeSelect />
-      <DatePickerComponent />
-      <SelectCompanyBox />
+    <div className="outerDiv">
+      <div className="sideBar">
+      </div>
+      <div className="header">
+        <div className="element">
+            <SearchInputBox />
+        </div>
+        <div className="element">
+            <SelectCompanyBox />
+        </div>
+        <div className="element">
+            <DocumentTypeSelect />
+        </div>
+        <div className="element">
+            <DatePickerComponent />
+        </div>
+      </div>
     </div>
   );
 }
